504fa62dff2bf94d3d58d0ff54d6c1ab8aee87c7
yepdoleslie <lesliebrielle.yepdomonthe@esprit.tn>
2023-10-07 21:23:31 +0200

    ajout commentaire

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -24,7 +24,7 @@ public class User implements Serializable {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;   
-	
+	//premier test
 	private String firstName; 
 	
 	private String lastName;
